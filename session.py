138b479de14bf54696246cac6e2d410810a56ada
alemi <me@alemi.dev>
2021-04-02 10:59:36 +0200

    No more global, made a session class

---
+++ session.py
@@ -0,0 +1,58 @@
+import subprocess
+import os
+
+from signal import SIGINT
+
+import ffmpeg
+
+from bot import alemiBot
+
+class Session:
+	def __init__(self):
+		self.spotify_process = None
+		self.ffmpeg_process = None
+		self.group_call = None
+		self.muted = False
+		self.logfile = None
+
+	def start(self, device_name="SpotyRobot", device_type="speaker", quiet=True):
+		username = alemiBot.config.get("spotify", "username", fallback=None)
+		password = alemiBot.config.get("spotify", "password", fallback=None)
+		try:
+			os.mkfifo("data/raw-fifo")
+			os.mkfifo("data/music-fifo")
+		except FileExistsError:
+			pass
+		if quiet:
+			self.logfile = open("data/spotify.log", "w")
+		self.spotify_process = subprocess.Popen(
+			["./data/librespot", "--name", device_name, "--backend", "pipe",
+			 "--device", "./data/raw-fifo", "-u", username, "-p", password, "--passthrough"],
+			stderr=subprocess.STDOUT, stdout=self.logfile # if it's none it inherits stdout from parent
+		)
+		self.ffmpeg_process = ffmpeg.input("data/raw-fifo").output(
+			"data/music-fifo",
+			format='s16le',
+			acodec='pcm_s16le',
+			ac=2,
+			ar='48k'
+		).overwrite_output().run_async(quiet=quiet)
+
+	def stop(self):
+		try:
+			self.spotify_process.send_signal(SIGINT)
+			self.spotify_process.wait(timeout=5)
+		except TimeoutError:
+			self.spotify_process.kill()
+		try:
+			self.ffmpeg_process.send_signal(SIGINT)
+			self.ffmpeg_process.wait(timeout=5)
+		except TimeoutError:
+			self.ffmpeg_process.kill()
+		os.remove("data/music-fifo")
+		os.remove("data/raw-fifo")
+		if self.logfile is not None:
+			self.logfile.close()
+			self.logfile = None
+
+sess = Session()
--- spotify.py
+++ spotify.py
@@ -18,103 +18,79 @@ from util.command import filterCommand
 
 from plugins.help import HelpCategory
 
+from plugins.spotyrobot.session import sess
+
 import logging
 logger = logging.getLogger(__name__)
 
 HELP = HelpCategory("SPOTIFY")
-group_call = None
-decoder_instance = None
-spotify_instance = None
-muted = False
-
-spoty_logfile = None
-
-async def prep_radio(device_name="SpotyRobot", quiet=True):
-	# TODO make a state instance and not this many global!!
-	global spotify_instance
-	global decoder_instance
-	global group_call
-	global spoty_logfile
-	username = alemiBot.config.get("spotify", "username", fallback=None)
-	password = alemiBot.config.get("spotify", "password", fallback=None)
-	try:
-		os.mkfifo("data/raw-fifo")
-		os.mkfifo("data/music-fifo")
-	except FileExistsError:
-		pass
-	if quiet:
-		spoty_logfile = open("data/spotify.log", "w")
-	spotify_instance = await asyncio.create_subprocess_exec(
-		f"./data/librespot", "--name", device_name, "--backend", "pipe", "--device", "./data/raw-fifo", "-u", username, "-p", password, "--passthrough",
-		stderr=asyncio.subprocess.STDOUT, stdout=spoty_logfile # if it's none it inherits stdout from parent
-	)
-	decoder_instance = ffmpeg.input("data/raw-fifo").output(
-		"data/music-fifo",
-		format='s16le',
-		acodec='pcm_s16le',
-		ac=2,
-		ar='48k'
-	).overwrite_output().run_async(quiet=quiet)
 
 @alemiBot.on_message(is_superuser & filters.voice_chat_members_invited)
 async def invited_to_voice_chat(client, message):
-	global group_call
 	try:
-		await prep_radio()
-		group_call = GroupCall(client, path_to_log_file='')
-		await group_call.start(message.chat.id)
-		group_call.input_filename = "data/music-fifo"
-		group_call.restart_playout()
+		sess.start()
+		sess.group_call = GroupCall(client, path_to_log_file='')
+		await sess.group_call.start(message.chat.id)
+		sess.group_call.input_filename = "data/music-fifo"
+		sess.group_call.restart_playout()
 	except:
-		logger.exception("Error in .leave command")
+		logger.exception("Error while joining voice chat")
 
-HELP.add_help("join", "join call and start radio", "join call and start radio")
+HELP.add_help("join", "join call and start radio",
+				"join voice call and start librespot + ffmpeg. It will show up as " +
+				"a spotify device, you should select it and play music on it. It will " +
+				"be played by the bot. You can specify the device name with `-n` and the " +
+				"device type with `-t`. Available types are : `[ computer, tablet, " +
+				"smartphone, speaker, tv, avr, stb, audiodongle ]`.", args="[-n <name>] [-t <type>]")
 @alemiBot.on_message(is_superuser & filterCommand("join", list(alemiBot.prefixes), options={
-	"devicename" : ["-n", "--name"]
+	"name" : ["-n", "--name"],
+	"type" : ["-t", "--type"],
 }))
 async def join_call_start_radio(client, message):
-	global group_call
 	try:
-		await prep_radio(device_name=message.command["devicename"] 
-									if "devicename" in message.command else "SpotyRobot")
-		group_call = GroupCall(client, path_to_log_file='')
-		await group_call.start(message.chat.id)
-		group_call.input_filename = "data/music-fifo"
-		group_call.restart_playout()
+		devicename = message.command["name"] if "name" in message.command else "SpotyRobot"
+		devicetype = message.command["type"] if "type" in message.command else "speaker"
+		sess.start(device_name=devicename, device_type=devicetype)
+		sess.group_call = GroupCall(client, path_to_log_file='')
+		await sess.group_call.start(message.chat.id)
+		sess.group_call.input_filename = "data/music-fifo"
+		sess.group_call.restart_playout()
 		await edit_or_reply(message, "` → ` Connected")
 	except Exception as e:
 		logger.exception("Error in .leave command")
 		await edit_or_reply(message, "`[!] → ` " + str(e))
 
-HELP.add_help("leave", "stop radio and leave call", "stop radio and leave call")
+HELP.add_help("leave", "stop radio and leave call",
+				"will first stop playback, then try to terminate both librespot and ffmpeg, " +
+				"and then leave the voice call.")
 @alemiBot.on_message(is_superuser & filterCommand("leave", list(alemiBot.prefixes)))
 async def stop_radio(client, message):
-	global spotify_instance
-	global decoder_instance
-	global group_call
-	global spoty_logfile
 	try:
-		await group_call.stop()
-		spotify_instance.kill() # I would love to stop this more gracefully but sometimes it just hangs!
-		await spotify_instance.wait()
-		decoder_instance.send_signal(SIGINT)
-		decoder_instance.wait()
-		os.remove("data/music-fifo")
-		os.remove("data/raw-fifo")
+		sess.group_call.stop_playout()
+		sess.stop()
+		await sess.group_call.stop()
 		await edit_or_reply(message, "` → ` Disconnected")
-		if spoty_logfile is not None:
-			spoty_logfile.close()
-			spoty_logfile = None
 	except Exception as e:
 		logger.exception("Error in .leave command")
 		await edit_or_reply(message, "`[!] → ` " + str(e))
 	await client.set_offline()
 
-HELP.add_help("volume", "join call and start radio", "join call and start radio")
+HELP.add_help("volume", "set player volume",
+				"make bot set its own call volume (must have rights to manage voice call)")
 @alemiBot.on_message(is_superuser & filterCommand("volume", list(alemiBot.prefixes)))
 async def volume(client, message):
 	if "cmd" not in message.command:
 		return await edit_or_reply(message, "`[!] → ` No value given")
 	val = int(message.command["cmd"][1])
-	await group_call.set_my_volume(val)
+	await sess.group_call.set_my_volume(val)
 	await edit_or_reply(message, f"` → ` Volume set to {val}")
+
+HELP.add_help("mute", "toggle player mute", "make bot mute/unmute itself")
+@alemiBot.on_message(is_superuser & filterCommand("mute", list(alemiBot.prefixes)))
+async def mute_call(client, message):
+	sess.muted = not sess.muted
+	sess.group_call.set_is_mute(sess.muted)
+	if sess.muted:
+		await edit_or_reply(message, f"` → ` Muted")
+	else:
+		await edit_or_reply(message, f"` → ` Unmuted")
