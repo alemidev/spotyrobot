f1309dcaa41f373c9f6e57552813b2f331cf89b2
alemi <me@alemi.dev>
2021-04-04 03:41:55 +0200

    better progress bar

--- common.py
+++ common.py
@@ -6,8 +6,11 @@ def join_artists(artists):
 
 def format_track(track):
 	return  f"**{join_artists(track['artists'])}** - " + \
-			f"[{track['name']}]({track['external_urls']['spotify']}) (*{track['album']['name']}*)"
+			f"[{track['name']}]({track['external_urls']['spotify']})"
+
+def format_time(ms):
+	return f"{(ms//1000)//60}:{(ms//1000)%60}"
 
 def progress_bar(curr, tot, length=20):
 	index = int((curr*length)/tot)
-	return "─"*index + "|" + (length-index-1)*"─"
+	return f"`{format_time(curr)}` {'─'*index}|{'─'*(length-index-1)} `{format_time(tot)}`"
